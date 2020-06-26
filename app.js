32db41075a1a080611ee7aec488553b3f61d800c
Junaid Khan <57318339+Junaidkhan1998@users.noreply.github.com>
2020-06-26 21:14:33 +0500

    cal done

---
+++ app.js
@@ -0,0 +1,24 @@
+function getNumber(num){
+    var result = document.getElementById("result")
+    result.value += num
+}
+
+function clearResult(){
+    var result = document.getElementById("result")
+    
+    result.value = ""
+    
+    
+}
+
+// function allClearResult(){
+//     var result = document.getElementById("result")
+//     result.value = ""
+// }
+
+function getResult(){
+    var result = document.getElementById("result")
+    result.value = eval(result.value)
+}
+
+
